4e498f8d0d685b0bc78d78ae65d3116911358bd5
Ron Howe <ronhowe@hotmail.com>
2021-08-18 19:04:00 -0400

    Added Hyper-V, Windows, Windows Update Scripts

---
+++ hyper-v.ps1
@@ -0,0 +1,149 @@
+throw "Safety Net"
+
+#region Prerequisites
+
+# Requires Windows PowerShell
+if ($PSVersionTable.PSEdition -ne "Desktop") {
+    Write-Error "Requires Windows PowerShell" -ErrorAction Stop
+}
+
+# Requires Run As Administrator
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Error "Not running as Administrator." -ErrorAction Stop
+}
+
+# Requires Hyper-V Module
+Get-Module -Name "Hyper-V" -ListAvailable
+Import-Module -Name "Hyper-V"
+
+# Requires xHyper-V DSC Resource
+Get-Module -Name "xHyper-V" -ListAvailable
+Find-Module -Name "xHyper-V"
+Install-Module -Name "xHyper-V"
+
+# Requires Unrestricted Execution Policy
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+#endregion Prerequisites
+
+#region Spin Up
+
+Configuration "SpinUp" {
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $VMName,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $IPAddress
+    )
+
+    Import-DscResource -ModuleName "xHyper-V"
+
+    Node "localhost" {
+
+        xVHD $VMName {
+            Name             = $VMName
+            Ensure           = "Present"
+            Path             = "D:\Hyper-V\Virtual Hard Disks"
+            Generation       = "VHDX"
+            MaximumSizeBytes = 40GB
+        }
+
+        xVMHyperV $VMName {
+            Name                        = $VMName
+            Ensure                      = "Present"
+            DependsOn                   = "[xVHD]$VMName"
+            ProcessorCount              = 4
+            MinimumMemory               = 4GB
+            VhdPath                     = "D:\Hyper-V\Virtual Hard Disks\$VMName.vhdx"
+            EnableGuestService          = $true
+            AutomaticCheckpointsEnabled = $false
+        }
+
+        xVMDvdDrive NewVMDvdDriveISO {
+            Ensure             = "Present"
+            VMName             = $VMName
+            ControllerNumber   = 1
+            ControllerLocation = 0
+            Path               = "C:\Users\ronhowe\Downloads\Windows Server 2019.iso"
+            DependsOn          = "[xVMHyperV]$VMName"
+        }
+        
+        xVMNetworkAdapter $VMName {
+            Id             = $VMName
+            Name           = $VMName
+            Ensure         = "Present"
+            DependsOn      = "[xVMHyperV]$VMName"
+            SwitchName     = "Default Switch"
+            VMName         = $VMName
+            NetworkSetting = xNetworkSettings {
+                IpAddress      = $IPAddress
+                Subnet         = "255.255.240.0"
+                DefaultGateway = "172.18.48.1"
+                DnsServer      = "172.18.48.1"
+            }
+        }
+    }
+}
+
+Push-Location $env:TEMP
+
+SpinUp -VMName "DC01" -IPAddress "172.18.61.4" -OutputPath ./SpinUp
+Start-DscConfiguration -Path ./SpinUp -Verbose -Wait -Force
+
+SpinUp -VMName "SQL01" -IPAddress "172.18.61.5" -OutputPath ./SpinUp
+Start-DscConfiguration -Path ./SpinUp -Verbose -Wait -Force
+
+SpinUp -VMName "WEB01" -IPAddress "172.18.61.6" -OutputPath ./SpinUp
+Start-DscConfiguration -Path ./SpinUp -Verbose -Wait -Force
+
+#endregion Spin Up
+
+#region Spin Down
+
+Configuration "SpinDown" {
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $VMName
+    )
+
+    Import-DscResource -ModuleName "xHyper-V"
+
+    Node "localhost" {
+
+        xVHD $VMName {
+            Name             = $VMName
+            Ensure           = "Absent"
+            Path             = "D:\Hyper-V\Virtual Hard Disks"
+            Generation       = "VHDX"
+            MaximumSizeBytes = 40GB
+        }
+
+        xVMHyperV $VMName {
+            Name           = $VMName
+            Ensure         = "Absent"
+            DependsOn      = "[xVHD]$VMName"
+            ProcessorCount = 4
+            MinimumMemory  = 4GB
+            VhdPath        = "D:\Hyper-V\Virtual Hard Disks\$VMName.vhdx"
+        }
+    }
+}
+
+Push-Location $env:TEMP
+
+SpinDown -VMName "DC01" -OutputPath ./SpinDown
+Start-DscConfiguration -Path ./SpinDown -Verbose -Wait -Force
+
+SpinDown -VMName "SQL01" -OutputPath ./SpinDown
+Start-DscConfiguration -Path ./SpinDown -Verbose -Wait -Force
+
+SpinDown -VMName "WEB01" -OutputPath ./SpinDown
+Start-DscConfiguration -Path ./SpinDown -Verbose -Wait -Force
+
+#endregion Spin Down
