f36c0b12d2a31d532e806e9a55936672914143d7
Ron Howe <ronhowe@hotmail.com>
2021-11-12 16:09:03 -0500

    Fixed Credential Reference in Bootstrap Script

--- lab/Bootstrap-Node.ps1
+++ lab/Bootstrap-Node.ps1
@@ -9,6 +9,6 @@ $ProgressPreference = "SilentlyContinue"
 
 & "$PSScriptRoot\Install-DscResources.ps1" -ComputerName $ComputerName -Credential $MyConfiguration.VmAdministratorCredential
 
-& "$PSScriptRoot\Install-DscEncryptionCertificate.ps1" -ComputerName $ComputerName -Credential $MyConfiguration.VmAdministratorCredential -CertificatePfxPath "$env:TEMP\DscPrivateKey.pfx" -CertificatePfxPassword $CertificatePassword
+& "$PSScriptRoot\Install-DscEncryptionCertificate.ps1" -ComputerName $ComputerName -Credential $MyConfiguration.VmAdministratorCredential -CertificatePfxPath "$env:TEMP\DscPrivateKey.pfx" -CertificatePfxPassword $MyConfiguration.DscEncryptionCertPfxPassword
 
 & "$PSScriptRoot\Invoke-LocalConfigurationManagerDsc.ps1" -ComputerName $ComputerName -Credential $MyConfiguration.VmAdministratorCredential -CertificateFile "$env:TEMP\DscPublicKey.cer" -Thumbprint $MyConfiguration.DscEncryptionCertificateThumbprint
