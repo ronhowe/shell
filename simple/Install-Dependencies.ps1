990b2fb1c659d608690bf2b61215b5df121caaf8
Ron Howe <ronhowe@hotmail.com>
2021-11-15 14:45:27 -0500

    Added Install-GuestDependencies.ps1

--- simple/Install-Dependencies.ps1
+++
@@ -1,7 +0,0 @@
-Install-Module -Name "ActiveDirectoryCSDsc" -Scope CurrentUser -Repository "PSGallery" -Force
-Install-Module -Name "ActiveDirectoryDsc" -Scope CurrentUser -Repository "PSGallery" -Force
-Install-Module -Name "ComputerManagementDsc" -Scope CurrentUser -Repository "PSGallery" -Force
-Install-Module -Name "NetworkingDsc" -Scope CurrentUser -Repository "PSGallery" -Force
-Install-Module -Name "PSDscResources" -Scope CurrentUser -Repository "PSGallery" -Force
-Install-Module -Name "SqlServerDsc" -Scope CurrentUser -Repository "PSGallery" -Force
-Install-Module -Name "xHyper-V" -Scope CurrentUser -Repository "PSGallery" -Force
---
+++ simple/Install-GuestDependencies.ps1
@@ -0,0 +1,53 @@
+[CmdletBinding()]  
+param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+    [ValidateNotNullorEmpty()]
+    [string[]]
+    $ComputerName,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullorEmpty()]
+    [PSCredential]
+    $Credential,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullorEmpty()]
+    [string]
+    $PfxPath,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullorEmpty()]
+    [securestring]
+    $PfxPassword
+)
+begin {
+}
+process {
+    Write-Verbose "Getting PSSession"
+    $Session = New-PSSession -ComputerName $ComputerName -Credential $Credential
+
+    Write-Verbose "Copying PFX"
+    Copy-Item -Path $PfxPath -Destination "C:\DscPrivateKey.pfx" -ToSession $Session
+
+    Write-Verbose "Importing PFX"
+    Invoke-Command -Session $Session -ScriptBlock {
+        Import-PfxCertificate -FilePath "C:\DscPrivateKey.pfx" -CertStoreLocation "Cert:\LocalMachine\My" -Password $using:PfxPassword
+    }
+
+    Write-Verbose "Installing Modules"
+    Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock {
+        $ProgressPreference = "SilentlyContinue"
+        Install-PackageProvider -Name "Nuget" -Force -Verbose | Out-Null
+        Install-Module -Name "ActiveDirectoryCSDsc" -Repository "PSGallery" -Force -Verbose
+        Install-Module -Name "ActiveDirectoryDsc" -Repository "PSGallery" -Force -Verbose
+        Install-Module -Name "ComputerManagementDsc" -Repository "PSGallery" -Force -Verbose
+        Install-Module -Name "NetworkingDsc" -Repository "PSGallery" -Force -Verbose
+        Install-Module -Name "PSDscResources" -Repository "PSGallery" -Force -Verbose
+        Install-Module -Name "SqlServerDsc" -Repository "PSGallery" -Force -Verbose
+    }
+
+    Write-Verbose "Removing PSSession"
+    $Session | Remove-PSSession
+}
+end {
+}
